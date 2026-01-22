7e5cff06e0adeb39c0040f0e430d3e1afc9d9135
JANE-dot978 <gathujane97@gmail.com>
2026-01-22 16:45:08 +0300

    added a maintanance screen for the the landlord hostels maintanance

--- lib/screens/landlord/maintenance_screen.dart
+++ lib/screens/landlord/maintenance_screen.dart
@@ -1,12 +1,12 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// class MaintenanceScreen extends StatelessWidget {
-//   const MaintenanceScreen({super.key});
+class MaintenanceScreen extends StatelessWidget {
+  const MaintenanceScreen({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return const Center(
-//       child: Text('Maintenance requests will appear here'),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Maintenance requests will appear here'),
+    );
+  }
+}
