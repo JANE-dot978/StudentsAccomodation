52482209972307765a3283c99a43e81e52c5281b
JANE-dot978 <gathujane97@gmail.com>
2026-01-27 20:28:38 +0300

    added the setting screen for the landlords ability to udjust his peofie and dashboard

--- lib/screens/landlord/settings_screen.dart
+++ lib/screens/landlord/settings_screen.dart
@@ -1,24 +1,24 @@
-// import 'package:flutter/material.dart';
+import 'package:flutter/material.dart';
 
-// class SettingsScreen extends StatelessWidget {
-//   const SettingsScreen({super.key});
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(title: const Text("Settings")),
-//       body: ListView(
-//         children: const [
-//           ListTile(
-//             leading: Icon(Icons.lock),
-//             title: Text("Change Password"),
-//           ),
-//           ListTile(
-//             leading: Icon(Icons.notifications),
-//             title: Text("Notifications"),
-//           ),
-//         ],
-//       ),
-//     );
-//   }
-// }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Settings")),
+      body: ListView(
+        children: const [
+          ListTile(
+            leading: Icon(Icons.lock),
+            title: Text("Change Password"),
+          ),
+          ListTile(
+            leading: Icon(Icons.notifications),
+            title: Text("Notifications"),
+          ),
+        ],
+      ),
+    );
+  }
+}
