7925dbd15bcdea51f1d25d99b301bd508fa77958
JANE-dot978 <gathujane97@gmail.com>
2026-02-02 17:44:37 +0300

    added single room details screen for students

--- lib/screens/student/categories/single_room_screen.dart
+++ lib/screens/student/categories/single_room_screen.dart
@@ -1,27 +1,27 @@
-// import 'package:flutter/material.dart';
-// import 'package:provider/provider.dart';
-// import 'package:studentsaccomodations/providers/hostel_provider.dart';
-// import 'package:studentsaccomodations/widgets/hostels_cards.dart';
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:studentsaccomodations/providers/hostel_provider.dart';
+import 'package:studentsaccomodations/widgets/hostels_cards.dart';
 
-// class SingleRoomScreen extends StatelessWidget {
-//   const SingleRoomScreen({super.key});
+class SingleRoomScreen extends StatelessWidget {
+  const SingleRoomScreen({super.key});
 
-//   @override
-//   Widget build(BuildContext context) {
-//   final hostels = Provider.of<HostelProvider>(context)
-//     .hostels
-//     .where((h) => h.category.toLowerCase() == "single room")
-//     .toList();
+  @override
+  Widget build(BuildContext context) {
+  final hostels = Provider.of<HostelProvider>(context)
+    .hostels
+    .where((h) => h.category.toLowerCase() == "single room")
+    .toList();
 
 
-//     return Scaffold(
-//       appBar: AppBar(title: const Text("Single Rooms")),
-//       body: hostels.isEmpty
-//           ? const Center(child: Text("No single rooms available"))
-//           : ListView.builder(
-//               itemCount: hostels.length,
-//               itemBuilder: (_, i) => StyledHostelCard(hostel: hostels[i]),
-//             ),
-//     );
-//   }
-// }
+    return Scaffold(
+      appBar: AppBar(title: const Text("Single Rooms")),
+      body: hostels.isEmpty
+          ? const Center(child: Text("No single rooms available"))
+          : ListView.builder(
+              itemCount: hostels.length,
+              itemBuilder: (_, i) => StyledHostelCard(hostel: hostels[i]),
+            ),
+    );
+  }
+}
